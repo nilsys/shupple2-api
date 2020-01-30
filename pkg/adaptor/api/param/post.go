2444c129a8b672c77c5b1be38f635916f70c8318
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-30 12:26:44 +0900

    [add] validate_method_2_post_param

--- pkg/adaptor/api/param/post.go
+++ pkg/adaptor/api/param/post.go
@@ -1,5 +1,10 @@
 package param
 
+import (
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/model"
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/model/serror"
+)
+
 type (
 	GetPost struct {
 		ID int `param:"id" validate:"required"`
@@ -23,8 +28,33 @@ type (
 	}
 )
 
+const findPostListDefaultPerPage = 10
+
+// いずれのクエリも飛んでこない場合 or sortの値が期待値以外の場合エラーを返す
 func (findPostListParam FindPostListParam) Validate() error {
 	if findPostListParam.AreaID == 0 && findPostListParam.SubAreaID == 0 && findPostListParam.SubSubAreaID == 0 && findPostListParam.ThemeID == 0 && findPostListParam.HashTag == "" {
-		return
+		return serror.New(nil, serror.CodeInvalidParam, "Invalid find post list param")
 	}
+
+	if findPostListParam.SortBy != model.RANKING.Value() || findPostListParam.SortBy != model.NEW.Value() {
+		return serror.New(nil, serror.CodeInvalidParam, "Invalid find post list sortBy")
+	}
+
+	return nil
+}
+
+// PerPageがクエリで飛んで来なかった場合、デフォルト値である10を返す
+func (findPostListParam FindPostListParam) GetPerPage() int {
+	if findPostListParam.PerPage == 0 {
+		return findPostListDefaultPerPage
+	}
+	return findPostListParam.PerPage
+}
+
+// offSetを返す
+func (findPostListParam FindPostListParam) GetOffSet() int {
+	if findPostListParam.Page == 1 || findPostListParam.Page == 0 {
+		return 0
+	}
+	return findPostListParam.GetPerPage()*(findPostListParam.Page-1) + 1
 }
