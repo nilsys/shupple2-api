8147116ae8dfb1ee4548077dfe28e050529777a4
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-30 14:28:06 +0900

    [add] post_converter

--- pkg/adaptor/api/converter/post.go
+++ pkg/adaptor/api/converter/post.go
@@ -1,1 +1,19 @@
 package converter
+
+import (
+	"github.com/stayway-corp/stayway-media-api/pkg/adaptor/api/param"
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/model/query"
+)
+
+func ConvertFindPostListParamToQuery(param param.FindPostListParam) *query.FindPostListQuery {
+	return &query.FindPostListQuery{
+		AreaID:       param.AreaID,
+		SubAreaID:    param.SubAreaID,
+		SubSubAreaID: param.SubSubAreaID,
+		ThemeID:      param.ThemeID,
+		HashTag:      param.HashTag,
+		SortBy:       param.SortBy,
+		Limit:        param.GetLimit(),
+		OffSet:       param.GetOffSet(),
+	}
+}
