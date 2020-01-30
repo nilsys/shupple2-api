cc18ac527fb39e61d8f9db6fbb3f4e876bbabe41
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-30 22:17:46 +0900

    [fix] category_query_repo_func

--- pkg/domain/repository/category_query.go
+++ pkg/domain/repository/category_query.go
@@ -3,6 +3,5 @@ package repository
 import "github.com/stayway-corp/stayway-media-api/pkg/domain/entity"
 
 type CategoryQueryRepository interface {
-	FindAreaByIDs(ids ...int) ([]*entity.Category, error)
-	FindThemeByIDs(ids ...int) ([]*entity.Category, error)
+	FindByIDs(ids ...int) ([]*entity.Category, error)
 }
