f70687833915798e21867e6a93b3cea2a3413f5c
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-30 17:03:50 +0900

    [add] find_func_2_post_repo

--- pkg/domain/repository/post_query.go
+++ pkg/domain/repository/post_query.go
@@ -1,7 +1,12 @@
 package repository
 
-import "github.com/stayway-corp/stayway-media-api/pkg/domain/entity"
+import (
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/entity"
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/model/query"
+)
 
 type PostQueryRepository interface {
 	FindByID(id int) (*entity.Post, error)
+	// TODO: 命名
+	FindByParams(query *query.FindPostListQuery) ([]*entity.Post, error)
 }
