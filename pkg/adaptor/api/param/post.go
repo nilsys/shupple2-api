e507845860563d3b6cbef36ebfc3d970536e0964
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-30 11:33:13 +0900

    [add] find_post_param

--- pkg/adaptor/api/param/post.go
+++ pkg/adaptor/api/param/post.go
@@ -7,6 +7,24 @@ type (
 
 	StorePostParam struct {
 		Title string
-		Body string
+		Body  string
+	}
+
+	// 記事取得API
+	FindPostListParam struct {
+		AreaID       int    `query:"areaId"`
+		SubAreaID    int    `query:"subAreaId"`
+		SubSubAreaID int    `query:"subSubAreaId"`
+		ThemeID      int    `query:"themeId"`
+		HashTag      string `query:"hashTag"`
+		SortBy       string `query:"sortBy"`
+		Page         int    `query:"page"`
+		PerPage      int    `query:"perPage"`
 	}
 )
+
+func (findPostListParam FindPostListParam) Validate() error {
+	if findPostListParam.AreaID == 0 && findPostListParam.SubAreaID == 0 && findPostListParam.SubSubAreaID == 0 && findPostListParam.ThemeID == 0 && findPostListParam.HashTag == "" {
+		return
+	}
+}
