48655814dd310c8001c6a9dd9b90a94472c36c29
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-31 19:15:07 +0900

    [fix] sort_by_type

--- pkg/domain/model/query/post.go
+++ pkg/domain/model/query/post.go
@@ -1,5 +1,7 @@
 package query
 
+import "github.com/stayway-corp/stayway-media-api/pkg/domain/model"
+
 type (
 	// repositoryで使用する検索用構造体
 	FindPostListQuery struct {
@@ -8,7 +10,7 @@ type (
 		SubSubAreaID int
 		ThemeID      int
 		HashTag      string
-		SortBy       string
+		SortBy       model.SortBy
 		Limit        int
 		OffSet       int
 	}
