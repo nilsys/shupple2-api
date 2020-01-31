66628e79e888d6d5096375b13e21fe2d46554fbd
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-01-31 19:16:19 +0900

    [update] sort_by_enum

--- pkg/domain/model/post_enum.go
+++ pkg/domain/model/post_enum.go
@@ -8,6 +8,33 @@ const (
 	NEW
 )
 
+// stringからSortByを返す
+// デフォルトは新着順を表すNEW
+func NewSortBy(sortByStr string) SortBy {
+	switch sortByStr {
+	case RANKING.Value():
+		return RANKING
+	case NEW.Value():
+		return NEW
+	default:
+		return NEW
+	}
+}
+
+// queryのORDERで使用する値を返す
+// デフォルトは新着順を表すNEW
+func (sortBy SortBy) GetOrderQuery() string {
+	switch sortBy {
+	case RANKING:
+		return "favorite_count desc"
+	case NEW:
+		return "created_at desc"
+	default:
+		return "created_at desc"
+	}
+}
+
+// 文字列で扱う
 func (sortBy SortBy) Value() string {
 	switch sortBy {
 	case RANKING:
