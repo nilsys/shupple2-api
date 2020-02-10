f4ef168e79fed44376696087c1a74ae0adfbf985
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-02-10 12:45:41 +0900

    [clean] del_extra_repo_with_chg

--- pkg/domain/repository/post.go
+++ pkg/domain/repository/post.go
@@ -1,6 +1,9 @@
 package repository
 
-import "github.com/stayway-corp/stayway-media-api/pkg/domain/entity"
+import (
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/entity"
+	"github.com/stayway-corp/stayway-media-api/pkg/domain/model/query"
+)
 
 type (
 	PostCommandRepository interface {
@@ -9,5 +12,6 @@ type (
 
 	PostQueryRepository interface {
 		FindByID(id int) (*entity.Post, error)
+		FindListByParams(query *query.FindPostListQuery) ([]*entity.Post, error)
 	}
 )
