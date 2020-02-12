4dad728926c54a2c4b55214b20a765fa87eb32c2
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-02-12 11:18:00 +0900

    [update] post_response

--- pkg/adaptor/api/response/post.go
+++ pkg/adaptor/api/response/post.go
@@ -1,14 +1,15 @@
 package response
 
 // フロント返却用Post
-type Post struct {
-	ID               int      `json:"id"`
-	Thumbnail        string   `json:"thumbnail"`
-	AreaCategories   []string `json:"areaCategories"`
-	ThemeCategories  []string `json:"themeCategories"`
-	Title            string   `json:"title"`
-	CreatorThumbnail string   `json:"creatorThumbnail"`
-	CreatorName      string   `json:"creatorName"`
-	LikeCount        int      `json:"likeCount"`
-	UpdatedAt        string   `json:"updatedAt"`
-}
+type (
+	Post struct {
+		ID              int        `json:"id"`
+		Thumbnail       string     `json:"thumbnail"`
+		AreaCategories  []Category `json:"areaCategories"`
+		ThemeCategories []Category `json:"themeCategories"`
+		Title           string     `json:"title"`
+		Creator         Creator    `json:"creator"`
+		LikeCount       int        `json:"likeCount"`
+		UpdatedAt       string     `json:"updatedAt"`
+	}
+)
