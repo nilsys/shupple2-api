b4223ee1c9e47b7c76504a63be3269e0a17bd7b0
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-02-12 11:13:56 +0900

    [del] factory_&_dto_4_post

--- pkg/domain/dto/post.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-import "github.com/stayway-corp/stayway-media-api/pkg/domain/entity"
-
-type PostDetail struct {
-	Post       *entity.Post
-	Categories []*entity.Category
-	User       *entity.User
-}
