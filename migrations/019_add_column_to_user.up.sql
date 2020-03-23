30863ca1b82bb0a902b3a7c648f50cced7464b41
kobaken <skobaken@gmail.com>
2020-03-23 22:25:27 +0900

    migrationミスを修正 (#166)

--- migrations/019_add_column_to_user.up.sql
+++ migrations/019_add_column_to_user.up.sql
@@ -4,5 +4,8 @@ ALTER TABLE user
     ADD facebook_url  VARCHAR(255) DEFAULT NULL COMMENT 'FacebookURL',
     ADD instagram_url  VARCHAR(255) DEFAULT NULL COMMENT 'InstagramURL',
     ADD twitter_url  VARCHAR(255) DEFAULT NULL COMMENT 'TwitterURL',
-    ADD living_area  VARCHAR(255) DEFAULT NULL COMMENT '居住エリア',
-    ADD CONSTRAINT UNIQUE INDEX user_uid(uid)
+    ADD living_area  VARCHAR(255) DEFAULT NULL COMMENT '居住エリア';
+
+UPDATE user SET uid = concat("userdummy", id);
+
+ALTER TABLE user ADD CONSTRAINT UNIQUE INDEX user_uid(uid);
--- migrations/022_add_column_to_review.up.sql
+++ migrations/022_add_column_to_review.up.sql
@@ -1,5 +1,9 @@
 ALTER TABLE review
+    MODIFY tourist_spot_id BIGINT UNSIGNED,
+    MODIFY inn_id BIGINT UNSIGNED;
+
+ALTER TABLE review
     ADD comment_count INT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'レビューにひもづくコメント数' AFTER accompanying,
     ADD FULLTEXT KEY(body) WITH PARSER NGRAM,
     ADD CONSTRAINT review_user_id FOREIGN KEY(user_id) REFERENCES user(id),
-    ADD CONSTRAINT review_tourist_spot_id FOREIGN KEY(tourist_spot_id) REFERENCES tourist_spot(id)
+    ADD CONSTRAINT review_tourist_spot_id FOREIGN KEY(tourist_spot_id) REFERENCES tourist_spot(id);
