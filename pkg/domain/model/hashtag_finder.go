6c513c566b9279f6b800bf416aceb2e9d8596690
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-03-24 14:11:57 +0900

    Update hashtag_finder.go (#174)

--- pkg/domain/model/hashtag_finder.go
+++ pkg/domain/model/hashtag_finder.go
@@ -2,9 +2,14 @@ package model
 
 import "regexp"
 
-var re = regexp.MustCompile(`#\S+`)
+var re = regexp.MustCompile(`#(\S+)`)
 
 // 文字列の中からhashtagを探す
 func FindHashtags(str string) []string {
-	return re.FindAllString(str, -1)
+	hashtags := make([]string, 0)
+	matches := re.FindAllStringSubmatch(str, -1)
+	for _, s := range matches {
+		hashtags = append(hashtags, s[1])
+	}
+	return hashtags
 }
