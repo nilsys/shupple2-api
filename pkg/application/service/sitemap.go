1009e2a342ebd227a43b8e16f6913fef18856ae7
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-08-28 16:14:53 +0900

    [fix] sitemap (#575)

--- pkg/application/service/sitemap.go
+++ pkg/application/service/sitemap.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+
+	path2 "path"
 
 	"github.com/google/wire"
 	"github.com/pkg/errors"
@@ -22,6 +25,11 @@ type (
 	}
 )
 
+const (
+	cfProjectPathPrefix    = "/projects"
+	fixTargetCfProjectPath = "/project"
+)
+
 var SitemapServiceSet = wire.NewSet(
 	wire.Struct(new(SitemapServiceImpl), "*"),
 	wire.Bind(new(SitemapService), new(*SitemapServiceImpl)),
@@ -29,7 +37,9 @@ var SitemapServiceSet = wire.NewSet(
 
 func (s *SitemapServiceImpl) Show(path string) ([]byte, string, error) {
 	origURL := s.WordpressConfig.BaseURL
+	fmt.Println(origURL)
 	origURL.Path = path // prefixがpathに入っているのでJoinではなく代入で良い
+	fmt.Println(origURL)
 
 	file, err := s.WordpressQueryRepository.FetchResource(origURL.String())
 	if err != nil {
@@ -43,6 +53,15 @@ func (s *SitemapServiceImpl) Show(path string) ([]byte, string, error) {
 	}
 
 	body = bytes.ReplaceAll(body, s.WordpressConfig.BaseURL.Byte(), s.MediaConfig.BaseURL.Byte())
+
+	// CfProjectは/tourismのprefixが必要ない
+	cfProjectURL := s.MediaConfig.BaseURL
+	cfProjectURL.Path = cfProjectPathPrefix
+
+	// 修正対象
+	fixTargetCfProjectURL := s.MediaConfig.BaseURL
+	fixTargetCfProjectURL.Path = path2.Join(fixTargetCfProjectURL.Path, fixTargetCfProjectPath)
+	body = bytes.ReplaceAll(body, fixTargetCfProjectURL.Byte(), cfProjectURL.Byte())
 
 	return body, file.ContentType, nil
 }
