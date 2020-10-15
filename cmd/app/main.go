94d0f9751432c093fff4c1bb42ce71d604cdd7a7
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-15 11:42:08 +0900

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/uma-co82/shupple2-api/pkg/adaptor/api"
-	"github.com/uma-co82/shupple2-api/pkg/adaptor/infrastructure/repository"
-	"github.com/uma-co82/shupple2-api/pkg/adaptor/logger"
 	"github.com/uma-co82/shupple2-api/pkg/config"
 
 	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/pkg/errors"
 )
 
 func main() {
@@ -30,26 +26,27 @@ type App struct {
 }
 
 func run() error {
-	app, err := InitializeApp(config.DefaultConfigFilePath)
-	if err != nil {
-		return errors.Wrap(err, "failed to initialize app")
-	}
-	logger.Configure(app.Config.Logger)
-
-	if app.Config.Migrate.Auto {
-		if err := repository.MigrateUp(app.Config.Database, app.Config.Migrate.FilesDir); err != nil {
-			return errors.Wrap(err, "failed to migrate up")
-		}
-	}
-
-	app.Echo.Debug = app.Config.IsDev()
-	app.Echo.HTTPErrorHandler = api.CreateErrorHandler(app.Config.Env)
-	app.Echo.Use(middleware.RequestID())
-	app.Echo.Use(middleware.Logger())
-	app.Echo.Use(middleware.CORS())
-	setRoutes(app)
-
-	return app.Echo.Start(":3000")
+	//app, err := InitializeApp(config.DefaultConfigFilePath)
+	//if err != nil {
+	//	return errors.Wrap(err, "failed to initialize app")
+	//}
+	//logger.Configure(app.Config.Logger)
+	//
+	//if app.Config.Migrate.Auto {
+	//	if err := repository.MigrateUp(app.Config.Database, app.Config.Migrate.FilesDir); err != nil {
+	//		return errors.Wrap(err, "failed to migrate up")
+	//	}
+	//}
+	//
+	//app.Echo.Debug = app.Config.IsDev()
+	//app.Echo.HTTPErrorHandler = api.CreateErrorHandler(app.Config.Env)
+	//app.Echo.Use(middleware.RequestID())
+	//app.Echo.Use(middleware.Logger())
+	//app.Echo.Use(middleware.CORS())
+	//setRoutes(app)
+	//
+	//return app.Echo.Start(":3000")
+	return nil
 }
 
 func setRoutes(app *App) {
