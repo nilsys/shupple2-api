a3eb3a8458b47382ca22bd9c37f02eead0b45f68
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-15 17:27:39 +0900

    Create user.go

---
+++ pkg/domain/entity/user.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/uma-co82/shupple2-api/pkg/domain/model"
+)
+
+type (
+	UserTiny struct {
+		ID         int `gorm:"primary_key"`
+		FirebaseID string
+		Name       string
+		Email      string
+		Birthdate  time.Time
+		Gender     model.Gender
+	}
+)
