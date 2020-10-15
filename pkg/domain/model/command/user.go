b54408aec4c7c8fe793d61fcaee52a2eae214914
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-16 01:26:02 +0900

    Create user.go

---
+++ pkg/domain/model/command/user.go
@@ -0,0 +1,17 @@
+package command
+
+import (
+	"time"
+
+	"github.com/uma-co82/shupple2-api/pkg/domain/model"
+)
+
+type (
+	StoreUser struct {
+		Name      string
+		Email     string
+		Birthdate time.Time
+		Profile   string
+		Gender    model.Gender
+	}
+)
