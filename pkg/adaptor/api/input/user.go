87efa9463b84ba2fb43482c9d4eaf59b61b19a2e
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-21 16:16:36 +0900

    Update user.go

--- pkg/adaptor/api/input/user.go
+++ pkg/adaptor/api/input/user.go
@@ -4,11 +4,12 @@ import "github.com/uma-co82/shupple2-api/pkg/domain/model"
 
 type (
 	RegisterUser struct {
-		FirebaseToken string       `json:"firebaseToken"`
-		Name          string       `json:"name"`
-		Email         string       `json:"email"`
-		Birthdate     model.Date   `json:"birthdate"`
-		Profile       string       `json:"profile"`
-		Gender        model.Gender `json:"gender"`
+		FirebaseToken string           `json:"firebaseToken"`
+		Name          string           `json:"name"`
+		Email         string           `json:"email"`
+		Birthdate     model.Date       `json:"birthdate"`
+		Profile       string           `json:"profile"`
+		Gender        model.Gender     `json:"gender"`
+		Prefecture    model.Prefecture `json:"prefecture"`
 	}
 )
