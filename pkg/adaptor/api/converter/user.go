e618a47caa4ec5d33b2d531f79b902359c38cfc3
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-23 01:39:53 +0900

    Update user.go

--- pkg/adaptor/api/converter/user.go
+++ pkg/adaptor/api/converter/user.go
@@ -3,6 +3,11 @@ package converter
 import (
 	"time"
 
+	"github.com/uma-co82/shupple2-api/pkg/domain/model"
+
+	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/output"
+	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
+
 	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/input"
 	"github.com/uma-co82/shupple2-api/pkg/domain/model/command"
 )
@@ -25,6 +30,28 @@ func (c Converters) ConvertRegisterUserInput2Cmd(in *input.RegisterUser) command
 		Gender:         in.Gender,
 		Prefecture:     in.Prefecture,
 		MatchingReason: in.MatchingReason,
+		Images:         images,
+	}
+}
+
+func (c Converters) ConvertUser2Output(user *entity.User) output.User {
+	images := make([]output.UserImage, len(user.Images))
+	for i, image := range user.Images {
+		images[i] = output.UserImage{
+			Priority: image.Priority,
+			URL:      image.URL(c.filesURL()),
+		}
+	}
+
+	return output.User{
+		ID:             user.ID,
+		Name:           user.Name,
+		Birthdate:      model.Date(user.Birthdate),
+		Profile:        user.Profile,
+		Gender:         user.Gender,
+		Prefecture:     user.Prefecture,
+		MatchingReason: user.MatchingReason,
+		IsMatching:     user.IsMatching,
 		Images:         images,
 	}
 }
