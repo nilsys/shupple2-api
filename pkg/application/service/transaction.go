691937186fcacc71c9b2c0844a514f551de45209
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 17:53:34 +0900

    Create transaction.go

---
+++ pkg/application/service/transaction.go
@@ -0,0 +1,13 @@
+package service
+
+import "context"
+
+type TransactionService interface {
+	Do(f func(ctx context.Context) error) error
+}
+
+type TransactionServiceForTest struct{}
+
+func (s TransactionServiceForTest) Do(f func(ctx context.Context) error) error {
+	return f(context.Background())
+}
