ced7eb98b196f07e0aea9179860d26c161987e81
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 17:53:14 +0900

    Update enum.go

--- pkg/domain/model/enum.go
+++ pkg/domain/model/enum.go
@@ -8,7 +8,7 @@ ENUM(Transaction = 1)
 type ContextKey int
 
 /*
-ENUM(Male, Female)
+ENUM(Male = 1, Female)
 */
 type Gender int
 
@@ -16,3 +16,10 @@ type Gender int
 ENUM(Hokkaido = 1, Aomori, Iwate, Miyagi, Akita, Yamagata, Hukushima, Ibaragi, Totigi, Gunma, Saitama, Tiba, Tokyo, Kanagawa, Nigata, Toyama, Ishikawa, Hukui, Yamanashi, Nagano, Gihu, Sizuoka, Aiti, Mie, Shiga, Kyoto, Osaka, Hyogo, Nara, Wakayama, Tottori, Shimane, Okayama, Hiroshima, Yamaguchi, Tokushima, Kagawa, Ehime, Koti, Hukuoka, Saga, Nagasaki, Kumamoto, Oita, Miyazaki, Kagoshima, Okinawa)
 */
 type Prefecture int
+
+func (x Gender) Reverse() Gender {
+	if x == GenderMale {
+		return GenderFemale
+	}
+	return GenderMale
+}
