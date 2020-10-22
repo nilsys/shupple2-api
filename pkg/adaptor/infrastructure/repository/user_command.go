6cf0533fd7e15736469e7ec6f9b3639db238c7d4
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 23:52:45 +0900

    Update user_command.go

--- pkg/adaptor/infrastructure/repository/user_command.go
+++ pkg/adaptor/infrastructure/repository/user_command.go
@@ -24,3 +24,34 @@ func (r *UserCommandRepositoryImpl) Store(ctx context.Context, user *entity.User
 	}
 	return nil
 }
+
+func (r *UserCommandRepositoryImpl) StoreUserImages(ctx context.Context, images []*entity.UserImage) error {
+	db := r.DB(ctx)
+	for _, image := range images {
+		if err := db.Save(image).Error; err != nil {
+			return errors.Wrap(err, "failed store user_image")
+		}
+	}
+	return nil
+}
+
+func (r *UserCommandRepositoryImpl) StoreUserMatchingHistory(ctx context.Context, history *entity.UserMatchingHistory) error {
+	if err := r.DB(ctx).Save(history).Error; err != nil {
+		return errors.Wrap(err, "failed store user_matching_history")
+	}
+	return nil
+}
+
+func (r *UserCommandRepositoryImpl) UpdateForMatchingByIDs(ctx context.Context, ids []int) error {
+	if err := r.DB(ctx).Exec("UPDATE user SET is_matching = true WHERE id IN (?)", ids).Error; err != nil {
+		return errors.Wrap(err, "failed update user.is_matching = true")
+	}
+	return nil
+}
+
+func (r *UserCommandRepositoryImpl) UpdateForNotMatchingByID(ctx context.Context, id int) error {
+	if err := r.DB(ctx).Exec("UPDATE user SET is_matching = false WHERE id = ?", id).Error; err != nil {
+		return errors.Wrap(err, "failed update user.is_matching = false")
+	}
+	return nil
+}
