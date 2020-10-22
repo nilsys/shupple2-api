db731a96934beec40132145662ecbba34d64d41c
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 18:31:34 +0900

    Update user.go

--- pkg/domain/repository/user.go
+++ pkg/domain/repository/user.go
@@ -13,10 +13,11 @@ type (
 		FindByFirebaseID(id string) (*entity.UserTiny, error)
 		FindByID(id int) (*entity.UserTiny, error)
 		FindMatchingUserByID(id int) (*entity.UserTiny, error)
-		FindAvailableMatchingUser(gender model.Gender, id int) (*entity.UserTiny, error)
+		FindAvailableMatchingUser(gender model.Gender, reason model.MatchingReason, id int) (*entity.UserTiny, error)
 	}
 
 	UserCommandRepository interface {
 		Store(ctx context.Context, user *entity.UserTiny) error
+		StoreUserMatchingHistory(ctx context.Context, history *entity.UserMatchingHistory) error
 	}
 )
