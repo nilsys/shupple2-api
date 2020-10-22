51f44970ae6fdbbfe836142746dc6be054993cc9
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 18:31:43 +0900

    Update user.go

--- pkg/domain/entity/user.go
+++ pkg/domain/entity/user.go
@@ -3,19 +3,48 @@ package entity
 import (
 	"time"
 
+	"github.com/uma-co82/shupple2-api/pkg/domain/model/command"
+
 	"github.com/uma-co82/shupple2-api/pkg/domain/model"
 )
 
 type (
 	UserTiny struct {
-		ID         int `gorm:"primary_key"`
-		FirebaseID string
-		Name       string
-		Email      string
-		Birthdate  time.Time
-		Profile    string
-		Gender     model.Gender
-		Prefecture model.Prefecture
-		IsMatching bool
+		ID             int `gorm:"primary_key"`
+		FirebaseID     string
+		Name           string
+		Email          string
+		Birthdate      time.Time
+		Profile        string
+		Gender         model.Gender
+		Prefecture     model.Prefecture
+		MatchingReason model.MatchingReason
+		IsMatching     bool
+	}
+
+	UserMatchingHistory struct {
+		UserID         int `gorm:"primary_key"`
+		MatchingUserID int `gorm:"primary_key"`
 	}
 )
+
+func NewUserTinyFromCmd(cmd command.StoreUser, firebaseID string) *UserTiny {
+	return &UserTiny{
+		FirebaseID:     firebaseID,
+		Name:           cmd.Name,
+		Email:          cmd.Email,
+		Birthdate:      cmd.Birthdate,
+		Profile:        cmd.Profile,
+		Gender:         cmd.Gender,
+		Prefecture:     cmd.Prefecture,
+		MatchingReason: cmd.MatchingReason,
+		IsMatching:     false,
+	}
+}
+
+func NewUserMatchingHistory(userID, matchingUserID int) *UserMatchingHistory {
+	return &UserMatchingHistory{
+		UserID:         userID,
+		MatchingUserID: matchingUserID,
+	}
+}
