1e2918b139705e004f3521fa580b4e3662d76f01
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 17:53:03 +0900

    Update code_enum.go

--- pkg/domain/model/serror/code_enum.go
+++ pkg/domain/model/serror/code_enum.go
@@ -20,35 +20,20 @@ const (
 	CodeUnauthorized
 	// CodeForbidden is a Code of type Forbidden
 	CodeForbidden
-	// CodeInvalidCategoryType is a Code of type InvalidCategoryType
-	CodeInvalidCategoryType
-	// CodePayAgentError is a Code of type PayAgentError
-	CodePayAgentError
-	// CodeDuplicateCard is a Code of type DuplicateCard
-	CodeDuplicateCard
-	// CodeUnsupportedMedia is a Code of type UnsupportedMedia
-	CodeUnsupportedMedia
-	// CodeDuplicateReport is a Code of type DuplicateReport
-	CodeDuplicateReport
-	// CodeExpired is a Code of type Expired
-	CodeExpired
+	// CodeNotMatching is a Code of type NotMatching
+	CodeNotMatching
 )
 
-const _CodeName = "UndefinedInvalidParamNotFoundImportDeletedUnauthorizedForbiddenInvalidCategoryTypePayAgentErrorDuplicateCardUnsupportedMediaDuplicateReportExpired"
+const _CodeName = "UndefinedInvalidParamNotFoundImportDeletedUnauthorizedForbiddenNotMatching"
 
 var _CodeMap = map[Code]string{
-	0:  _CodeName[0:9],
-	1:  _CodeName[9:21],
-	2:  _CodeName[21:29],
-	3:  _CodeName[29:42],
-	4:  _CodeName[42:54],
-	5:  _CodeName[54:63],
-	6:  _CodeName[63:82],
-	7:  _CodeName[82:95],
-	8:  _CodeName[95:108],
-	9:  _CodeName[108:124],
-	10: _CodeName[124:139],
-	11: _CodeName[139:146],
+	0: _CodeName[0:9],
+	1: _CodeName[9:21],
+	2: _CodeName[21:29],
+	3: _CodeName[29:42],
+	4: _CodeName[42:54],
+	5: _CodeName[54:63],
+	6: _CodeName[63:74],
 }
 
 // String implements the Stringer interface.
@@ -60,18 +45,13 @@ func (x Code) String() string {
 }
 
 var _CodeValue = map[string]Code{
-	_CodeName[0:9]:     0,
-	_CodeName[9:21]:    1,
-	_CodeName[21:29]:   2,
-	_CodeName[29:42]:   3,
-	_CodeName[42:54]:   4,
-	_CodeName[54:63]:   5,
-	_CodeName[63:82]:   6,
-	_CodeName[82:95]:   7,
-	_CodeName[95:108]:  8,
-	_CodeName[108:124]: 9,
-	_CodeName[124:139]: 10,
-	_CodeName[139:146]: 11,
+	_CodeName[0:9]:   0,
+	_CodeName[9:21]:  1,
+	_CodeName[21:29]: 2,
+	_CodeName[29:42]: 3,
+	_CodeName[42:54]: 4,
+	_CodeName[54:63]: 5,
+	_CodeName[63:74]: 6,
 }
 
 // ParseCode attempts to convert a string to a Code
