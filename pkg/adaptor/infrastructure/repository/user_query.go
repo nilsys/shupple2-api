1adb7ce8b1fc495f9013adcfc34c32543d4dd95c
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-22 18:31:47 +0900

    Update user_query.go

--- pkg/adaptor/infrastructure/repository/user_query.go
+++ pkg/adaptor/infrastructure/repository/user_query.go
@@ -42,10 +42,10 @@ func (r *UserQueryRepositoryImpl) FindMatchingUserByID(id int) (*entity.UserTiny
 	return &row, nil
 }
 
-func (r *UserQueryRepositoryImpl) FindAvailableMatchingUser(gender model.Gender, id int) (*entity.UserTiny, error) {
+func (r *UserQueryRepositoryImpl) FindAvailableMatchingUser(gender model.Gender, reason model.MatchingReason, id int) (*entity.UserTiny, error) {
 	var row entity.UserTiny
 	if err := r.DB.
-		Where("is_matching = false AND gender = ? AND id NOT IN (?) AND id NOT IN (SELECT matching_user_id FROM user_matching_history WHERE user_id = ?)", gender.Reverse(), id, id).
+		Where("is_matching = false AND gender = ? AND matching_reason = ? AND id NOT IN (?) AND id NOT IN (SELECT matching_user_id FROM user_matching_history WHERE user_id = ?)", gender.Reverse(), reason, id, id).
 		First(&row).Error; err != nil {
 		return nil, errors.Wrap(err, "failed find user")
 	}
