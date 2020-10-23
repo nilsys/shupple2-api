24d0cf8f5b245d202a29711bd40c4c8ba520946e
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-23 20:10:11 +0900

    Update wire.go

--- cmd/app/wire.go
+++ cmd/app/wire.go
@@ -6,6 +6,8 @@ import (
 	"github.com/google/wire"
 	"github.com/labstack/echo/v4"
 	"github.com/uma-co82/shupple2-api/pkg/adaptor/api"
+	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/converter"
+	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/middleware"
 	"github.com/uma-co82/shupple2-api/pkg/adaptor/infrastructure/repository"
 	"github.com/uma-co82/shupple2-api/pkg/application/service"
 	"github.com/uma-co82/shupple2-api/pkg/config"
@@ -28,9 +30,11 @@ func InitializeApp(configFilePath config.FilePath) (*App, error) {
 		echo.New,
 		controllerSet,
 		serviceSet,
+		middleware.AuthorizeSet,
 		wire.Struct(new(App), "*"),
 		config.GetConfig,
 		wire.FieldsOf(new(*config.Config), "AWS"),
+		converter.ConverterSet,
 		repository.RepositoriesSet,
 	)
 
