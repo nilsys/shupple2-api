8be4996335e0635089c8a9e5c4e88d8211473499
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-29 13:37:47 +0900

    Update s3.go

--- pkg/domain/repository/aws/s3.go
+++ pkg/domain/repository/aws/s3.go
@@ -1,9 +1,12 @@
 package aws
 
-import "github.com/aws/aws-sdk-go/service/s3/s3manager"
+import (
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
 
 type (
 	S3CommandRepository interface {
 		Upload(input *s3manager.UploadInput) error
+		Delete(key string) error
 	}
 )
