da234f56bcef300f11f4d39a8804dfda486cc683
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-30 19:02:40 +0900

    Create arrange_schedule_request_command.go

---
+++ pkg/adaptor/infrastructure/repository/arrange_schedule_request_command.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/google/wire"
+	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
+	"github.com/uma-co82/shupple2-api/pkg/domain/repository"
+)
+
+type ArrangeScheduleRequestCommandRepositoryImpl struct {
+	DAO
+}
+
+var ArrangeScheduleRequestCommandRepositorySet = wire.NewSet(
+	wire.Struct(new(ArrangeScheduleRequestCommandRepositoryImpl), "*"),
+	wire.Bind(new(repository.ArrangeScheduleRequestCommandRepository), new(*ArrangeScheduleRequestCommandRepositoryImpl)),
+)
+
+func (r *ArrangeScheduleRequestCommandRepositoryImpl) Store(ctx context.Context, request *entity.ArrangeScheduleRequest) error {
+	if err := r.DB(ctx).Save(request).Error; err != nil {
+		return errors.Wrap(err, "failed store arrange_schedule_request")
+	}
+	return nil
+}
