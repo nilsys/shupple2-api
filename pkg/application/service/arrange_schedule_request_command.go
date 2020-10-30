d46ad9ed3df7cd08b1df7efa9cc04de2e24774f9
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-10-30 19:06:24 +0900

    Create arrange_schedule_request_command.go

---
+++ pkg/application/service/arrange_schedule_request_command.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
+	"github.com/uma-co82/shupple2-api/pkg/domain/model/command"
+	"github.com/uma-co82/shupple2-api/pkg/domain/repository"
+)
+
+type (
+	ArrangeScheduleRequestCommandService interface {
+		Store(cmd *command.StoreArrangeScheduleRequest, user *entity.UserTiny) error
+	}
+
+	ArrangeScheduleRequestCommandServiceImpl struct {
+		repository.ArrangeScheduleRequestCommandRepository
+	}
+)
