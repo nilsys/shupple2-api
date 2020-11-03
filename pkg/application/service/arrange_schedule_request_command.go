e3d56a0cc830acac24268f62ceb184d7dd9cb994
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-03 22:11:23 +0900

    Update arrange_schedule_request_command.go

--- pkg/application/service/arrange_schedule_request_command.go
+++ pkg/application/service/arrange_schedule_request_command.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"context"
+
+	"github.com/pkg/errors"
 	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
 	"github.com/uma-co82/shupple2-api/pkg/domain/model/command"
+	"github.com/uma-co82/shupple2-api/pkg/domain/model/serror"
 	"github.com/uma-co82/shupple2-api/pkg/domain/repository"
 )
 
@@ -12,10 +16,23 @@ type (
 	}
 
 	ArrangeScheduleRequestCommandServiceImpl struct {
+		repository.UserQueryRepository
 		repository.ArrangeScheduleRequestCommandRepository
 	}
 )
 
 func (s *ArrangeScheduleRequestCommandServiceImpl) Store(cmd *command.StoreArrangeScheduleRequest, user *entity.UserTiny) error {
-	req := entity.ArrangeScheduleRequest{}
+	isMatchinged, err := s.UserQueryRepository.IsExistUserMatchingHistory(user.ID, cmd.MatchingUserID)
+	if err != nil {
+		return errors.Wrap(err, "failed find user_matching_history")
+	}
+
+	// マッチングしていないユーザーに送ろうとした場合
+	if !isMatchinged {
+		return serror.New(nil, serror.CodeNotMatching, "not matching")
+	}
+
+	req := entity.NewArrangeScheduleRequest(user.ID, cmd.MatchingUserID, cmd.Date, cmd.Remark)
+
+	return s.ArrangeScheduleRequestCommandRepository.Store(context.Background(), req)
 }
