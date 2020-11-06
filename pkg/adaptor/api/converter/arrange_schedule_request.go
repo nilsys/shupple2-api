21bcc0b010c88a9bb0492d0f8906ed6421b6173b
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-06 18:50:13 +0900

    Update arrange_schedule_request.go

--- pkg/adaptor/api/converter/arrange_schedule_request.go
+++ pkg/adaptor/api/converter/arrange_schedule_request.go
@@ -3,6 +3,10 @@ package converter
 import (
 	"time"
 
+	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/output"
+	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
+	"github.com/uma-co82/shupple2-api/pkg/domain/model"
+
 	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/input"
 	"github.com/uma-co82/shupple2-api/pkg/domain/model/command"
 )
@@ -12,5 +16,28 @@ func (c Converters) ConvertStoreArrangeScheduleRequestInputToCmd(input *input.St
 		MatchingUserID: input.MatchingUserID.ID,
 		Date:           time.Time(input.DateTime),
 		Remark:         input.Remark,
+	}
+}
+
+func (c Converters) ConvertArrangeScheduleRequestList2Output(reqs []*entity.ArrangeScheduleRequest) []output.ArrangeScheduleRequest {
+	resolve := make([]output.ArrangeScheduleRequest, len(reqs))
+
+	for i, req := range reqs {
+		resolve[i] = c.ConvertArrangeScheduleRequest2Output(req)
+	}
+
+	return resolve
+}
+
+func (c Converters) ConvertArrangeScheduleRequest2Output(req *entity.ArrangeScheduleRequest) output.ArrangeScheduleRequest {
+	return output.ArrangeScheduleRequest{
+		ID:                  req.ID,
+		UserID:              req.UserID,
+		MatchingUserID:      req.MatchingUserID,
+		DateTime:            model.DateTime(req.DateTime),
+		Remark:              req.Remark,
+		MatchingUserApprove: req.MatchingUserApprove,
+		User:                c.ConvertUser2Output(req.User),
+		MatchingUser:        c.ConvertUser2Output(req.MatchingUser),
 	}
 }
