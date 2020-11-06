f38b629aa2f52355eb15e9d72988498d0c06e4b0
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-06 18:50:15 +0900

    Update arrange_schedule_request_query.go

--- pkg/adaptor/api/arrange_schedule_request_query.go
+++ pkg/adaptor/api/arrange_schedule_request_query.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/google/wire"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/converter"
 	"github.com/uma-co82/shupple2-api/pkg/application/service"
 	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
@@ -18,4 +21,19 @@ var ArrangeScheduleRequestQueryControllerSet = wire.NewSet(
 )
 
 func (c *ArrangeScheduleRequestQueryController) ShowReceiveList(ctx echo.Context, user *entity.UserTiny) error {
+	resolve, err := c.ArrangeScheduleRequestQueryService.ShowReceiveList(user)
+	if err != nil {
+		return errors.Wrap(err, "failed show receive arrange schedule request")
+	}
+
+	return ctx.JSON(http.StatusOK, c.ConvertArrangeScheduleRequestList2Output(resolve))
+}
+
+func (c *ArrangeScheduleRequestQueryController) ShowSendList(ctx echo.Context, user *entity.UserTiny) error {
+	resolve, err := c.ArrangeScheduleRequestQueryService.ShowSendList(user)
+	if err != nil {
+		return errors.Wrap(err, "failed show send arrange schedule request")
+	}
+
+	return ctx.JSON(http.StatusOK, c.ConvertArrangeScheduleRequestList2Output(resolve))
 }
