5380dd3ca422718bfef9fa33dab7ed011c3b6849
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-06 18:17:37 +0900

    Update arrange_schedule_request_query.go

--- pkg/application/service/arrange_schedule_request_query.go
+++ pkg/application/service/arrange_schedule_request_query.go
@@ -2,16 +2,18 @@ package service
 
 import (
 	"github.com/google/wire"
+	"github.com/uma-co82/shupple2-api/pkg/domain/entity"
 	"github.com/uma-co82/shupple2-api/pkg/domain/repository"
 )
 
 type (
 	ArrangeScheduleRequestQueryService interface {
+		ShowReceiveList(user *entity.UserTiny) ([]*entity.ArrangeScheduleRequest, error)
 	}
 
 	ArrangeScheduleRequestQueryServiceImpl struct {
 		repository.UserQueryRepository
-		repository.ArrangeScheduleRequestCommandRepository
+		repository.ArrangeScheduleRequestQueryRepository
 	}
 )
 
@@ -19,3 +21,11 @@ var ArrangeScheduleRequestQueryServiceSet = wire.NewSet(
 	wire.Struct(new(ArrangeScheduleRequestQueryServiceImpl), "*"),
 	wire.Bind(new(ArrangeScheduleRequestQueryService), new(*ArrangeScheduleRequestQueryServiceImpl)),
 )
+
+func (s *ArrangeScheduleRequestQueryServiceImpl) ShowReceiveList(user *entity.UserTiny) ([]*entity.ArrangeScheduleRequest, error) {
+	return s.ArrangeScheduleRequestQueryRepository.FindByMatchingUserID(user.ID)
+}
+
+func (s) ()  {
+	
+}
