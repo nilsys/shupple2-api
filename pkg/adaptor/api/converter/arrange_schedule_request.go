fa76ff5af0e9eee04b726959dffb59dd1262cd6a
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-06 17:55:23 +0900

    Update arrange_schedule_request.go

--- pkg/adaptor/api/converter/arrange_schedule_request.go
+++ pkg/adaptor/api/converter/arrange_schedule_request.go
@@ -1,5 +1,16 @@
 package converter
 
-func (c Converters) ConvertArrangeScheduleRequest() {
+import (
+	"time"
 
+	"github.com/uma-co82/shupple2-api/pkg/adaptor/api/input"
+	"github.com/uma-co82/shupple2-api/pkg/domain/model/command"
+)
+
+func (c Converters) ConvertStoreArrangeScheduleRequestInputToCmd(input *input.StoreArrangeScheduleRequest) *command.StoreArrangeScheduleRequest {
+	return &command.StoreArrangeScheduleRequest{
+		MatchingUserID: input.MatchingUserID.ID,
+		Date:           time.Time(input.DateTime),
+		Remark:         input.Remark,
+	}
 }
