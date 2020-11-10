c6451506b4f39ec7588325fb8555dc3e37d36991
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-10 15:33:04 +0900

    Update arrange_schedule_request.go

--- pkg/adaptor/api/input/arrange_schedule_request.go
+++ pkg/adaptor/api/input/arrange_schedule_request.go
@@ -1,11 +1,15 @@
 package input
 
-import "github.com/uma-co82/shupple2-api/pkg/domain/model"
+import (
+	"github.com/uma-co82/shupple2-api/pkg/domain/model"
+	"gopkg.in/guregu/null.v3"
+)
 
 type (
 	StoreArrangeScheduleRequest struct {
 		MatchingUserID IDParam
 		DateTime       model.DateTime `json:"dateTime"`
 		Remark         string         `json:"remark"`
+		StartNow       null.Bool      `json:"startNow"`
 	}
 )
